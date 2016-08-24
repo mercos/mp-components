ea1224b64cf7bedf3739a9b42812b37a31ef5775
Juan Carlos Ramires <jcramires851@gmail.com>
2016-08-24 19:09:30 -0300

    Mudanças CSS no componente AlertaInline

--- src/components/ListagemComSubcategorias/AlertaInline.jsx
+++ src/components/ListagemComSubcategorias/AlertaInline.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './AlertaInline.scss'
 
 export default class AlertaInline extends React.Component {
   render() {
     return (
-      <div>
+      <div className={styles.linhaAlerta}>
         Deseja excluir?
         <span onClick={this.props.handleConfirmar}> Sim</span> | <span onClick={this.props.handleCancelar}>Não</span>
       </div>
