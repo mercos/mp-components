abd7ff0acfa956346d2c097fdcf275010c2301b3
Juan Carlos Ramires <jcramires851@gmail.com>
2016-08-24 16:53:04 -0300

    Alterações linter

--- src/components/ListagemComSubcategorias/OpcoesCategoria.jsx
+++ src/components/ListagemComSubcategorias/OpcoesCategoria.jsx
@@ -3,11 +3,15 @@ import styles from './OpcoesCategoria.scss'
 
 export default class OpcoesCategoria extends React.Component {
 
-    render() {
-        let visibilidade = this.props.exibir ? 'visible' : 'hidden'
+  render() {
+    const visibilidade = this.props.exibir ? 'visible' : 'hidden'
 
-        return (
-            <span className={styles.opcoes} style={{visibility: visibilidade}}>OpcoesCategoria</span>
-        )
-    }
+    return (
+      <span className={styles.opcoes} style={{ visibility: visibilidade }}>OpcoesCategoria</span>
+    )
+  }
+}
+
+OpcoesCategoria.propTypes = {
+  exibir: React.PropTypes.bool,
 }
