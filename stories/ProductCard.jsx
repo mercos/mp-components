b543e649f8b0a6f58490938f1ecd4160dbc53f5c
Jorge Klemm <jorge@meuspedidos.com.br>
2016-09-22 16:55:37 -0300

    Fixed ProductCard margins

--- stories/ProductCard.jsx
+++ stories/ProductCard.jsx
@@ -5,10 +5,24 @@ import ProductCard from '../src/components/ProductCard/ProductCard.jsx'
 
 storiesOf('ProductCard', module)
 
-  .add('default', () => (
+  .add('with image', () => (
     <ProductCard name="Nome do Produto" code="0101" unitOfMeasure="Unidade" imageURL="http://placehold.it/273x176" />
+  ))
+
+  .add('two cards with image', () => (
+    <div>
+      <ProductCard name="Produto 1" code="0101" unitOfMeasure="Unidade" imageURL="http://placehold.it/273x176" />
+      <ProductCard name="Produto 2" code="0202" unitOfMeasure="Unidade" imageURL="http://placehold.it/273x176" />
+    </div>
   ))
 
   .add('without image', () => (
     <ProductCard name="Nome do Produto" code="0101" unitOfMeasure="Unidade" imageURL="" />
+  ))
+
+  .add('two cards without image', () => (
+    <div>
+      <ProductCard name="Produto 1" code="0101" unitOfMeasure="Unidade" imageURL="" />
+      <ProductCard name="Produto 2" code="0202" unitOfMeasure="Unidade" imageURL="" />
+    </div>
   ))
