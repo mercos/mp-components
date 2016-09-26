f655227d969ec8e9526ce01d764f520a093a0e1a
Juan Carlos Ramires <jcramires851@gmail.com>
2016-09-26 15:52:18 -0300

    fix linter errors

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,7 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import cx from 'classnames'
-import classnames from 'classnames'
-import { applyContainerQuery } from 'react-container-query'
+import { applyContainerQuery } from 'react-container-query/dist/react-container-query.min'
 import styles from './ProductCard.scss'
 import ProductImage from '../ProductImage/ProductImage.jsx'
 import ProductDescription from '../ProductDescription/ProductDescription.jsx'
@@ -10,8 +9,12 @@ class ProductCardBase extends Component {
   render() {
     return (
       <div className={cx(styles.productCardContainer, this.props.containerQuery)} style={{ width: this.props.width }}>
-        <ProductImage imageURL={ this.props.imageURL }/>
-        <ProductDescription name={ this.props.name } code={ this.props.code } unitOfMeasure={ this.props.unitOfMeasure }/>
+        <ProductImage imageURL={this.props.imageURL} />
+        <ProductDescription
+          name={this.props.name}
+          code={this.props.code}
+          unitOfMeasure={this.props.unitOfMeasure}
+        />
       </div>
     )
   }
@@ -26,7 +29,7 @@ ProductCardBase.propTypes = {
   containerQuery: PropTypes.object,
 }
 
-let query = {}
+const query = {}
 
 query[styles.productCardDesktop] = {
   minWidth: 320,
