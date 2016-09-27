fb25d466b5fd84ed5e64f201940269da431fc8b1
Diego Faria <diegofndefaria@gmail.com>
2016-09-27 14:07:37 -0300

    added parameters for productImage

--- stories/ProductImage.js
+++ stories/ProductImage.js
@@ -5,11 +5,23 @@ import ProductImage from '../src/components/ProductImage/ProductImage.jsx'
 
 storiesOf('ProductImage', module)
 
-  .add('with image', () => (
+  .add('image', () => (
     <ProductImage imageURL="http://placehold.it/273x176" />
   ))
 
-  .add('with big image', () => (
+  .add('height parameter 400', () => (
+    <ProductImage height={400} imageURL="http://placehold.it/250x400" />
+  ))
+
+  .add('width parameter 400', () => (
+    <ProductImage width={400} imageURL="http://placehold.it/400x150" />
+  ))
+
+  .add('height and width parameter 400', () => (
+    <ProductImage width={400} height={400} imageURL="http://placehold.it/500x500" />
+  ))
+
+  .add('big image without size parameters', () => (
     <ProductImage imageURL="http://placehold.it/500x500" />
   ))
 
